20bce8421986bdbb8d082f03cc8d9370723051bc
shawn <shawnfeng610@gmail.com>
2022-09-25 20:44:40 +0800

    add FizzBuzz

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -3,7 +3,6 @@ public class FizzBuzz {
     public static void main(String[] args){
       
         for(int i=1; i<=100; i++){
-            
             if(i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
             }else if(i % 3 == 0){
@@ -13,7 +12,6 @@ public class FizzBuzz {
             }else{
                 System.out.println(i);
             }
-                
         }
 
     }
